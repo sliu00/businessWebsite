d0a6a2c33fe5086eb61ff8195e8634af85ec17d3
Sam Liu <63525488+sliu00@users.noreply.github.com>
2020-09-26 23:20:42 -0400

    Initial files

---
+++ nav.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("openNav").style.marginLeft = "0";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("openNav").style.marginLeft= "0";
+}
